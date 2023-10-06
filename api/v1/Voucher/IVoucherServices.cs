54745e52890d30522e4bcb9215c90eaf60b45b75
Loc IT <phamhuuloc.dev@gmail.com>
2023-10-06 09:12:38 +0700

    feat: get all vouchers #34

--- api/v1/Voucher/IVoucherServices.cs
+++ api/v1/Voucher/IVoucherServices.cs
@@ -4,5 +4,8 @@ using Movie_Server.Models;
 namespace Movie_Server.Services {
     public interface IVoucherServices {
         Task<ApiResponse> createNewVoucher(VoucherModel voucher);
+        Task<ApiResponse> updateVoucherInfo(VoucherModel voucher, string id );
+        Task<ApiResponse> getAllVouchers();
+        Task<ApiResponse> getVoucherById(string id);
     }
 } 
